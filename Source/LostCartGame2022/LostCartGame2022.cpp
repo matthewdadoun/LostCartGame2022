b38276435fa928e6427f39b678d116b7f64f76eb
Matthew Dadoun <33492474+matthewdadoun@users.noreply.github.com>
2022-11-24 19:46:30 -0500

    initial push

---
+++ Source/LostCartGame2022/LostCartGame2022.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LostCartGame2022.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LostCartGame2022, "LostCartGame2022" );
---
+++ Source/LostCartGame2022/LostCartGame2022GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LostCartGame2022GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LOSTCARTGAME2022_API ALostCartGame2022GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
