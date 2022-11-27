3c126e2649ea901762eef79944897adea2792fe6
Matthew Dadoun <33492474+matthewdadoun@users.noreply.github.com>
2022-11-26 20:17:47 -0500

    projectile collision 1st pass + updated collision settings on enemies

--- Source/LostCartGame2022/LProjectileBase.h
+++ Source/LostCartGame2022/LProjectileBase.h
@@ -36,19 +36,23 @@ public:
 	UPROPERTY(VisibleAnywhere, Category = "Components")
 	UAudioComponent* AudioComp;
 
-	UFUNCTION()
-	void OnActorHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+	
 
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+public:
+
+	UFUNCTION()
+	virtual void PostInitializeComponents() override;
+	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-	
-	//virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnProjectileBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 protected:
 	
